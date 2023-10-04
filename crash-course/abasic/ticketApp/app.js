f4490660fb92c4d9fc41b87998e981795609cfe3
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-10-04 15:36:45 +0600

    vue

---
+++ crash-course/abasic/ticketApp/app.js
@@ -0,0 +1,143 @@
+Vue.createApp({
+    data() {
+        return {
+            busSeats: [
+                [
+                    {
+                        name: 'A1',
+                        type: 'available',
+                        price: 800,
+                    },
+                    {
+                        name: 'A2',
+                        type: 'available',
+                        price: 800,
+                    },
+                    {
+                        name: 'A3',
+                        type: 'available',
+                        price: 800,
+                    },
+                    {
+                        name: 'A4',
+                        type: 'available',
+                        price: 800,
+                    }
+                ],
+                [
+                    {
+                        name: 'B1',
+                        type: 'available',
+                        price: 800,
+                    },
+                    {
+                        name: 'B2',
+                        type: 'available',
+                        price: 800,
+                    },
+                    {
+                        name: 'B3',
+                        type: 'available',
+                        price: 800,
+                    },
+                    {
+                        name: 'B4',
+                        type: 'available',
+                        price: 800,
+                    }
+                ],
+                [
+                    {
+                        name: 'C1',
+                        type: 'available',
+                        price: 800,
+                    },
+                    {
+                        name: 'C2',
+                        type: 'available',
+                        price: 800,
+                    },
+                    {
+                        name: 'C3',
+                        type: 'available',
+                        price: 800,
+                    },
+                    {
+                        name: 'C4',
+                        type: 'available',
+                        price: 800,
+                    }
+                ], 
+                [
+                    {
+                        name: 'D1',
+                        type: 'available',
+                        price: 700,
+                    },
+                    {
+                        name: 'D2',
+                        type: 'available',
+                        price: 700,
+                    },
+                    {
+                        name: 'D3',
+                        type: 'available',
+                        price: 700,
+                    },
+                    {
+                        name: 'D4',
+                        type: 'available',
+                        price: 700,
+                    }
+                ], 
+                [
+                    {
+                        name: 'E1',
+                        type: 'available',
+                        price: 650,
+                    },
+                    {
+                        name: 'E2',
+                        type: 'available',
+                        price: 650,
+                    },
+                    {
+                        name: 'E3',
+                        type: 'available',
+                        price: 650,
+                    },
+                    {
+                        name: 'E4',
+                        type: 'available',
+                        price: 650,
+                    }
+                ], 
+                [
+                    {
+                        name: 'F1',
+                        type: 'available',
+                        price: 600,
+                    },
+                    {
+                        name: 'F2',
+                        type: 'available',
+                        price: 600,
+                    },
+                    {
+                        name: 'F3',
+                        type: 'available',
+                        price: 600,
+                    },
+                    {
+                        name: 'F4',
+                        type: 'available',
+                        price: 600,
+                    }
+                ]
+            ],
+            available: "hover:bg-slate-300 cursor-pointer"
+            
+        }
+    },
+
+}).mount("#app");
