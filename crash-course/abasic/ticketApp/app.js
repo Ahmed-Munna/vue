aab9e44dbc712ec4a5c51b5c41af7d0ce4bc3189
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-10-05 11:25:44 +0600

    vue js

--- crash-course/abasic/ticketApp/app.js
+++ crash-course/abasic/ticketApp/app.js
@@ -93,7 +93,7 @@ Vue.createApp({
                 [
                     {
                         name: 'E1',
-                        type: 'available',
+                        type: 'booked',
                         price: 650,
                     },
                     {
@@ -135,8 +135,80 @@ Vue.createApp({
                     }
                 ]
             ],
-            available: "hover:bg-slate-300 cursor-pointer"
+            coupon: {
+                name: '100TKOFF',
+                discount: 100
+            },
+            couponCode: '',
+            discountCoupon: 0,
+            name: '',
+            phone: '',
             
+        }
+    },
+    computed: {
+        selectedSeats() {
+            let stored = [];
+            let totalCost = 0;
+            for(let i = 0; i < 6; i++) {
+                for(let j = 0; j < 4; j++) {
+                   if (this.busSeats[i][j].type == 'selected') {
+                        stored.push(this.busSeats[i][j])
+                    }
+                }
+            }
+
+            if (stored.length != 0) {
+                for (let i = 0; i < stored.length; i++) {
+                    totalCost += stored[i].price;
+                }
+            }
+            
+           return [stored, totalCost];
+        },
+    },
+    methods: {
+        selectSeat(index,seatName) {
+            let seatCol = this.busSeats[index].length;
+            for (let i = 0; i < seatCol; i++) {
+               if (
+                this.busSeats[index][i].name == seatName && 
+                this.busSeats[index][i].type != 'sold' && 
+                this.busSeats[index][i].type != 'booked'
+                ) {
+                    if (this.busSeats[index][i].type == 'selected' ) { 
+                        this.busSeats[index][i].type = 'available'
+                   } else {
+
+                       this.busSeats[index][i].type = 'selected'
+                   }
+               } 
+                
+            }
+        },
+        bookedSeats() {
+            if (this.name !== '' && this.phone !== '') {
+                for(let i = 0; i < 6; i++) {
+                    for(let j = 0; j < 4; j++) {
+                       if (this.busSeats[i][j].type == 'selected') {
+                            this.busSeats[i][j].type = 'booked';
+                            this.name = '';
+                            this.phone = '';
+                            this.couponCode = '';
+                       }
+                    }
+                }
+            } else {
+                alert("please type your name and number");
+            }
+        }
+    },
+    
+    watch: {
+        couponCode(newValue) {
+            if (newValue == this.coupon.name) {
+                this.discountCoupon = this.coupon.discount;
+            }
         }
     },
 
