3d91444e4dd7255796989ece026862cf5fd4dd67
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-10-10 07:58:39 +0600

    vue card

---
+++ crash-course/vue-card/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="grid place-items-center">
+    <div class="">
+      <img 
+      src="./assets/apple.png"
+      class="rounded w-96 h-60"
+      >
+    </div>
+    <div class="bg-slate-50 px-8 py-6 w-80 -mt-20 rounded-lg shadow">
+      <CardBody 
+      :title="title"
+      :price="price"
+      @BuyNow="BuyNowClick"
+      ></CardBody>
+    </div>
+  </div>
+</template>
+
+<script>
+import CardBody from './components/CardBody.vue';
+export default {
+  name: 'App',
+  components: {
+    CardBody
+  },
+  data() {
+    return {
+      title: 'I Phone 15 Pro max',
+      price: '$1900'
+    }
+  },
+  methods: {
+    BuyNowClick() {
+      console.log('Hello');
+    }
+  }
+
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ crash-course/vue-card/src/components/CardBody.vue
@@ -0,0 +1,22 @@
+<template lang="en">
+    <div>
+        <p class="bg-[#B4F3EC] px-3 py-1 w-14 rounded-full text-sm">NEW</p>
+        <h3 class="text-xl my-2 font-semibold uppercase leading-tight truncate"> {{title}} </h3>
+        <p class="my-2"> {{ price }} /wk</p>
+        <button class="bg-[#B4F3EC] mt-8 px-8 py-2 rounded-full uppercase font-semibold" @click="BuyNow">Buy Now</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'CardBody',
+    props: {title: String, price: String},
+    methods: {
+        BuyNow() {
+            this.$emit('BuyNow');
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
