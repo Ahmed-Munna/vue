ef1743fbb5fb99506738f4272611e1e51ab8ccfa
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-10-12 15:02:40 +0600

    vue

---
+++ learning vue/non-prop/src/components/TheNonProp.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div>
+        <div class="myClass">
+            <h2 v-bind="$attrs">TheNonProp</h2>
+            <button class="btn">Ok</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    inheritAttrs: false
+}
+</script>
+<style lang="">
+    
+</style>
