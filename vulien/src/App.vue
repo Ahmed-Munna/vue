1a4f46e0225e5063ce45a24d236030b6b7637664
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-10-15 17:01:29 +0600

    vulien

---
+++ vulien/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <vlSimpleBtn></vlSimpleBtn>
+  </div>
+</template>
+<script>
+import vlSimpleBtn from './components/vl-button/vl-simple-btn.vue';
+
+export default {
+  components: {
+    vlSimpleBtn
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ vulien/src/components/vl-button/vl-simple-btn.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <div> 
+        {{tagName}}
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        tagName: {
+            type: String,
+            default: 'button',
+
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
