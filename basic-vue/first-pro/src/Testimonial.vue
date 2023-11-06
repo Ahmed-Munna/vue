de4e04aa8326ff5497b7472ed81ba8a369c1a8f7
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-11-07 05:00:28 +0600

    vue

---
+++ basic-vue/first-pro/src/Testimonial.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2> {{ count }} </h2>
+        <button @click="count++">click</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
--- basic-vue/first-pro/src/main.js
+++ basic-vue/first-pro/src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+
 import App from './App.vue'
+import Testimonial from './Testimonial.vue'
 
 createApp(App).mount('#app')
+createApp(Testimonial).mount('#testimonial')
