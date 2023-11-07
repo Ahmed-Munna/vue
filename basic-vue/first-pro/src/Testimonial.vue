0682819928e64ca90add7e6f05a60c54ee5c096b
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-11-07 18:17:56 +0600

    vue

--- basic-vue/first-pro/src/Testimonial.vue
+++ basic-vue/first-pro/src/Testimonial.vue
@@ -2,6 +2,10 @@
     <div>
         <h2> {{ count }} </h2>
         <button @click="count++">click</button>
+        <p>{{ rawHTML }}</p>
+        <p v-html="rawHTML"></p>
+        <p v-bind:id="myID">Hello</p>
+        <button v-bind="multiBind">Button</button>
     </div>
 </template>
 <script>
@@ -9,10 +13,27 @@ export default {
     data() {
         return {
             count: 0,
+            rawHTML: '<span style="color:red">This should be red</span>',
+            myID: "myID",
+            multiBind: {
+                id: 'btn',
+                class: 'myBtn'
+            }
         }
     }
 }
 </script>
-<style lang="">
-    
+<style>
+    #myID {
+        background-color: black;
+        color: white;
+    }
+    #btn {
+        padding: 8px;
+    }
+    .myBtn {
+        background-color: darkcyan;
+        color: aliceblue;
+        font-weight: bold;
+    }
 </style>
