651a49e52c55dc2a3ad501a169061dbf19c0a3ce
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-11-08 18:14:29 +0600

    vue

--- basic-vue/first-pro/src/Testimonial.vue
+++ basic-vue/first-pro/src/Testimonial.vue
@@ -1,24 +1,70 @@
 <template>
     <div>
-        <h2> {{ count }} </h2>
-        <button @click="count++">click</button>
+        <h2> {{ counted }} </h2>
+        <button @click="count">click</button>
         <p>{{ rawHTML }}</p>
         <p v-html="rawHTML"></p>
         <p v-bind:id="myID">Hello</p>
         <button v-bind="multiBind">Button</button>
+
+        <div>
+            <input type="text" placeholder="Add Items" v-model="newItem" @keyup.enter="submit"> <button @click="submit">submit</button>
+            <ul>
+                <li v-for="allItem in allItems" @key="allItem.id">{{allItem.lable}}</li>
+            </ul>
+        </div>
+        <div>
+            <input type="text" placeholder="Add Items" v-model="myNewItem" @keyup.enter="mysubmit"> <button @click="mysubmit">submit</button>
+            <ul>
+                <li v-for="item in isSubmited" @key="item.id">{{item.lable}}</li>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
+import { nextTick } from 'vue';
 export default {
     data() {
         return {
-            count: 0,
+            counted: 0,
             rawHTML: '<span style="color:red">This should be red</span>',
             myID: "myID",
             multiBind: {
                 id: 'btn',
                 class: 'myBtn'
-            }
+            },
+            newItem: '',
+            allItems: [
+                {'id': 1, 'lable': 'hello'}
+            ],
+            myNewItem: '',
+            items: [],
+
+        }
+    },
+    computed: {
+        isSubmited() {
+            return this.items.reverse();
+        }
+    },
+    methods: {
+        submit() {
+            if (this.newItem == '') {
+                return;
+            } 
+            this.allItems.push({id: this.allItems.length + 1, lable: this.newItem});
+            this.newItem = '';
+        },
+        async count() {
+            console.log(this.counted);
+            this.counted++;
+            await nextTick();
+            console.log(this.counted)
+        },
+        mysubmit() {
+           
+            this.items.push({id: this.items.length + 1, lable: this.myNewItem});
+            this.myNewItem = '';
         }
     }
 }
@@ -35,5 +81,6 @@ export default {
         background-color: darkcyan;
         color: aliceblue;
         font-weight: bold;
+        margin-bottom: 30px;
     }
 </style>
