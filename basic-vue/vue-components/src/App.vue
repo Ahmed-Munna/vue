852792716ca6adcde789488e9bd6d18cfca0a496
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-11-10 21:43:54 +0600

    vue

--- basic-vue/vue-components/src/App.vue
+++ basic-vue/vue-components/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <h1>Hello</h1>
+    <CounterCompo></CounterCompo>
   </div>
 </template>
 <script>
+import CounterCompo from './components/CounterCompo.vue';
 export default {
-  
+  components: {
+    CounterCompo
+  }
 }
 </script>
 <style>
---
+++ basic-vue/vue-components/src/components/CounterCompo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>Count: {{ count }}</h2>
+        <button @click="increment">Increment</button>
+        <button @click="decrement">Decrement</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+        }
+    },
+    methods: {
+        increment() {
+            if (this.count < 10)
+            this.count ++;
+        },
+        decrement() {
+            if (this.count > 0)
+            this.count --;
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
