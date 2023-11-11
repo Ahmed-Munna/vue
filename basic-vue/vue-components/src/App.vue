d56b8a0434d4d017c9b4e4b40ce2e90cf5a73f6a
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-11-11 15:24:43 +0600

    vue

--- basic-vue/vue-components/src/App.vue
+++ basic-vue/vue-components/src/App.vue
@@ -2,13 +2,45 @@
   <div>
     <h1>Hello</h1>
     <CounterCompo></CounterCompo>
+    <!-- <CardCompo
+    v-for="card in cards" 
+    :title="card.title"
+    >
+    {{ card.discription }}
+  </CardCompo> -->
+   <button 
+   v-for="tab in tabs"
+   :key="tab"
+   @click="currentTab = tab"
+   >{{ tab }}</button>
+   <component :is="currentTab"></component>
   </div>
 </template>
 <script>
+import CardCompo from './components/CardCompo.vue';
 import CounterCompo from './components/CounterCompo.vue';
+import TabOne from './components/TabOne.vue';
+import TabThree from './components/TabThree.vue';
+import TabTwo from './components/TabTwo.vue';
 export default {
+  data() {
+    return {
+      cards: [
+        {title: 'Title-1', discription:'When a value is passed to a prop attribute, it becomes a property on that component instance. The value of that property is accessible within the template.'},
+        {title: 'Title-2', discription:'When a value is passed to a prop attribute, it becomes a property on that component instance. The value of that property is accessible within the template.'},
+        {title: 'Title-3', discription:'When a value is passed to a prop attribute, it becomes a property on that component instance. The value of that property is accessible within the template.'},
+        {title: 'Title-4', discription:'When a value is passed to a prop attribute, it becomes a property on that component instance. The value of that property is accessible within the template.'},
+      ],
+      currentTab: 'TabOne',
+      tabs: ['TabOne', 'TabTwo', 'TabThree'],
+    }
+  },
   components: {
-    CounterCompo
+    CounterCompo,
+    CardCompo,
+    TabOne,
+    TabTwo,
+    TabThree
   }
 }
 </script>
---
+++ basic-vue/vue-components/src/components/CardCompo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card-body">
+        <h2>{{ title }}</h2>
+        <p><slot/></p>
+    </div>
+</template>
+<script>
+export default {
+    props: ['title']
+}
+</script>
+<style>
+    .card-body{
+        width: 300px;
+        text-align: left;
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+        padding: 20px;
+        border-radius: 8px;
+    }
+</style>
