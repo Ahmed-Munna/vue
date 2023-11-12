cc58641657104e93178f38f756dca9c1d61076e3
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-11-12 22:04:19 +0600

    vue

---
+++ basic-vue/component-in-depth/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Card v-bind="cardInfo"  @counting="countingOne"></Card>
+  </div>
+</template>
+<script>
+import Card from './components/Card.vue';
+
+export default {
+  data() {
+    return {
+      cardInfo: {
+        title: 'Card Title One',
+        discription: 'when the parent property updates, it will flow down to the child, but not the other way around.',
+        count: 0,
+      },
+    }
+  },
+  methods: {
+    countingOne(n) {
+      this.cardInfo.count += n;
+    }
+  },
+  components: {
+    Card,
+  }
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ basic-vue/component-in-depth/src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+        <p>{{ discription }}</p>
+        <p>{{ myProperty }}</p>
+        {{ count }}
+        <button @click="$emit('counting', 2)">Count</button>
+        <button @click="submit">another</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            myProperty: 'What',
+            anotherCount: 0,
+        }
+    },
+    // emits: ['counting'],
+    props: {
+        title: String,
+        discription: String,
+        count: Number,
+    },
+    created() {
+        console.log(this.myProperty);
+    },
+    methods: {
+        submit() {
+            this.$emit('counting', 10);
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
