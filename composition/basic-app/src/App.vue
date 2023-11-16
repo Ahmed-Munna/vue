abff49360fc36e8a6d10d07d9d17d9e950aeed24
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-11-16 09:44:28 +0600

    vue

--- composition/basic-app/src/App.vue
+++ composition/basic-app/src/App.vue
@@ -1,19 +1,11 @@
 <template>
   <div>
-    <h2>{{ myName }}</h2>
-    <h2>Count: {{ count }}</h2>
-    <button @click="clickMe">Add</button>
+   
   </div>
 </template>
 <script setup>
-import { ref } from 'vue';
 
-const myName = ref("Hello World");
-let count = ref(0);
 
-function clickMe() {
-  count += 1;
-}
 </script>
 <style lang="">
   
---
+++ composition/basic-app/src/views/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Contact vue</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
